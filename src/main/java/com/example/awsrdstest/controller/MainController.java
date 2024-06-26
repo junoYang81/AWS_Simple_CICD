af7657171152f9525faae4543c3999b72ea1cdac
junhoYang81 <newman9811@gmail.com>
2024-06-26 15:49:23 +0900

    AWS-Simple-test

--- src/main/java/com/example/awsrdstest/controller/MainController.java
+++ src/main/java/com/example/awsrdstest/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/main")
     public String sayHello() {
-        return "Hello!";
+        return "Hello aws!";
     }
 
 }
